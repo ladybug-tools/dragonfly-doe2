89e29f430598ba9a1de9c9cecd96c3f52866c458
trevor <63176386+TfedUD@users.noreply.github.com>
2021-12-22 12:56:02 -0500

    adding assy conversions

---
+++ dragonfly_doe2/doe_project/assemblies/idf_2_inp_opaque.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from honeybee_energy.construction.opaque import OpaqueConstruction as op_cons
+from honebee_energy.material.opaque import EnergyMaterial, EnergyMaterialNoMass
+from honeybee_energy.constructionset import ConstructionSet
+from dragonfly.model import Model as DFModel
+from dragonfly_energy.properties import *
+
+
+class OpaqueConstrConvert:
+    def __init__(self):
+        self.constrs = []
+
+    @classmethod
+    def from_df_model(cls, df_model):
+        cls_ = cls()
+        for const in df_model.properties.dragonfly_energy.properties:
+            pass
