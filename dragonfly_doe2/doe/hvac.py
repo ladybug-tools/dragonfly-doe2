1bdcc1505b6a50bfc7291f43e6158f37f5ed8f84
trevor <63176386+TfedUD@users.noreply.github.com>
2021-12-30 08:41:06 -0500

    fix: Working on upgrading requirements

--- dragonfly_doe2/doe/hvac.py
+++ dragonfly_doe2/doe/hvac.py
@@ -2,16 +2,16 @@ from .utils import short_name
 from dataclasses import dataclass
 
 
-class HVAC(object):
+class HVAC:
     """Placeholder HVAC class.
        Currently will just return no hvac
     """
 
     def to_inp(self):
-        inp_str = '"Sys1 (SUM)" = SYSTEM\n  ' \
-            'TYPE             = SUM\n  ' \
-            'HEAT-SOURCE      = NONE\n  ' \
-            'SYSTEM-REPORTS   = NO\n  ..\n'
+        inp_str = '"Sys1 (SUM)" = SYSTEM\n' \
+            '   TYPE             = SUM\n' \
+            '   HEAT-SOURCE      = NONE\n' \
+            '   SYSTEM-REPORTS   = NO\n   ..\n'
         return inp_str
 
     def __repr__(self):
